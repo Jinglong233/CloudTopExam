00689dc0ff57e6c88d980abe27025592ee655287
jinglong <2496232853@qq.com>
2024-05-12 14:28:52 +0800

    添加列表分页方法

---
+++ exam_common/src/main/java/com/jl/project/entity/dto/UpdateMsgUserDTO.java
@@ -0,0 +1,32 @@
+package com.jl.project.entity.dto;
+
+import com.jl.project.annotation.VerifyParam;
+import com.jl.project.entity.po.MsgUser;
+
+/**
+ * @Author huawei
+ * @Date 2024-05-12 12:59
+ **/
+public class UpdateMsgUserDTO {
+
+    @VerifyParam(require = true)
+    private String id;
+    @VerifyParam(require = true)
+    private MsgUser msgUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public MsgUser getMsgUser() {
+        return msgUser;
+    }
+
+    public void setMsgUser(MsgUser msgUser) {
+        this.msgUser = msgUser;
+    }
+}
--- exam_common/src/main/java/com/jl/project/utils/CommonUtil.java
+++ exam_common/src/main/java/com/jl/project/utils/CommonUtil.java
@@ -1,13 +1,45 @@
 package com.jl.project.utils;
 
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.PageUtil;
+import com.jl.project.entity.query.SimplePage;
+import com.jl.project.entity.vo.PaginationResultVO;
+import com.jl.project.enums.PageSize;
 import com.jl.project.enums.QuLevel;
 import com.jl.project.enums.QuType;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class CommonUtil {
+
+    /**
+     * 分页方法
+     *
+     * @param list
+     * @param page
+     * @param <T>
+     * @return
+     */
+    public static <T> PaginationResultVO<T> paginate(List<T> list, SimplePage page) {
+        if (page.getPageNo() <= 0 || page.getPageNo() > list.size()) {
+            page.setPageNo(1);
+        }
+        if (page.getPageSize() == null || page.getPageSize() <= 0) {
+            page.setPageSize(PageSize.SIZE15.getSize());
+        }
+        PaginationResultVO<T> result = new PaginationResultVO<>();
+        int startIndex = (page.getPageNo() - 1) * page.getPageSize();
+        int endIndex = Math.min(startIndex + page.getPageSize(), list.size());
+        result.setList(list.subList(startIndex, endIndex));
+        result.setPageNo(page.getPageNo());
+        result.setPageSize(page.getPageSize());
+        result.setTotalCount(list.size());
+        result.setPageTotal(PageUtil.totalPage(list.size(), page.getPageSize()));
+        return result;
+    }
+
     public static String getRandomId() {
         return IdUtil.simpleUUID();
     }
